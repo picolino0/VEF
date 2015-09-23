d2e21b4887f3e562309ba95b50f5883b422ebf82
Ivo Huntjens <gamerkeeperson@gmail.com>
2015-09-23 16:14:15 +0200

    Test with video.

---
+++ Sandbox/SandboxVideo/VideoDemo.cs
@@ -0,0 +1,18 @@
+﻿using DirectX.Capture;
+using DShowNET;
+using System.Windows.Forms;
+
+namespace SandboxVideo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Filters filters = new Filters();
+            Capture capture = new Capture(filters.VideoInputDevices[0], filters.AudioInputDevices[0]);
+            capture.PreviewWindow = panel;
+            capture.Start();
+        }
+    }
+}
