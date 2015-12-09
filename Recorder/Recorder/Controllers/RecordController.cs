96337e8b671b0ab490aaa59aec66a378918e557d
Villermen <villermen@gmail.com>
2015-12-09 16:20:57 +0100

    MVC skeleton app
    With optimization and stuff

---
+++ Recorder/Recorder/Controllers/RecordController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Recorder.Controllers
+{
+    public class RecordController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Recorder/Recorder/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Recorder.MvcApplication" Language="C#" %>
